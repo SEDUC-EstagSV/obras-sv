fc8acb01b6ad3d7cafde7ca5ec11d98475a6ec9d
Equipe TI - Seduc SV <121825599+SeducSV@users.noreply.github.com>
2023-03-08 11:04:39 -0300

    Fix criacao de fornecedor, Add validacao

--- fornecedor/salvar-fornecedor.php
+++ fornecedor/salvar-fornecedor.php
@@ -2,6 +2,7 @@
 
 
 include_once('function-seduc.php');
+include_once('function-fornecedor.php');
 require('validator.php');
 redirecionamentoPorAutoridade(3);
 
@@ -10,9 +11,33 @@ switch ($_REQUEST["acaofornecedor"]) {
     case 'CadastrarFornecedor':
         $nm_Fornecedor = validateInput($_POST["nm_Fornecedor"]);
         $num_CNPJ = validateInput($_POST["num_CNPJ"]);
+        if(validar_cnpj($num_CNPJ) === false){
+            print "<script>alert('O CNPJ informado é inválido');
+                        window.history.go(-1);</script>";
+            exit();
+        }
         $ds_Email = validateInput($_POST["ds_Email"]);
         $ds_Endereco = validateInput($_POST["ds_Endereco"]);
-        $st_Fornecedor = validateInput($_POST["st_Fornecedor"]);
+
+        //verifica existencia de usuário
+        try{
+            $sqlVerifica = $conn->prepare("SELECT nm_Fornecedor, num_CNPJ, ds_Email FROM fornecedor WHERE ds_Email = ? OR num_CNPJ = ?");
+            $sqlVerifica->bind_param('ss', $ds_Email, $num_CNPJ);
+            $sqlVerifica->execute();
+            $resVerifica = $sqlVerifica->get_result();
+            
+            $rowVerifica = $resVerifica->fetch_object();
+            if($rowVerifica->ds_Email == $ds_Email || $rowVerifica->num_CNPJ == $num_CNPJ){
+                print "<script>alert('O fornecedor $rowVerifica->nm_Fornecedor já foi cadastrado');
+                        window.history.go(-1);</script>";
+                exit();
+            }
+        } catch (mysqli_sql_exception $e){
+            print "<script>alert('Ocorreu um erro interno ao registrar fornecedor');
+            window.history.go(-1);</script>";
+            criaLogErro($e);
+            exit();
+        }
 
         try{
             $sql = $conn->prepare("INSERT INTO fornecedor (nm_Fornecedor, num_CNPJ, ds_Email, ds_Endereco) 
@@ -43,18 +68,16 @@ switch ($_REQUEST["acaofornecedor"]) {
         $num_CNPJ = validateInput($_POST["num_CNPJ"]);
         $ds_Email = validateInput($_POST["ds_Email"]);
         $ds_Endereco = validateInput($_POST["ds_Endereco"]);
-        $st_Fornecedor = validateInput($_POST["st_Fornecedor"]);
 
         try{
             $sql = $conn->prepare("UPDATE fornecedor SET nm_Fornecedor = ?,
                                                         num_CNPJ = ?,
                                                         ds_Email = ?,
-                                                        ds_Endereco = ?, 
-                                                        st_Fornecedor = ?
+                                                        ds_Endereco = ? 
                                     WHERE
                                         cd_Fornecedor = ?");
     
-            $sql->bind_param('sssssi',$nm_Fornecedor, $num_CNPJ, $ds_Email, $ds_Endereco, $st_Fornecedor, $cd_Fornecedor);
+            $sql->bind_param('sssss',$nm_Fornecedor, $num_CNPJ, $ds_Email, $ds_Endereco, $cd_Fornecedor);
 
             $res = $sql->execute();
     
